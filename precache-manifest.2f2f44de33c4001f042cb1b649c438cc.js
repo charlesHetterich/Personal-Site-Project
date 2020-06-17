1d63ee8b87ed5b6f985fb66d0e899d3c8e4a2644
Charles Hetterich <hetterich.charles@gmail.com>
2020-06-16 23:59:29 -0400

    Updates

---
+++ precache-manifest.2f2f44de33c4001f042cb1b649c438cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29196eafafd65e73288c2c2ef730f43f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "667b3871e1bd758b933b",
+    "url": "/static/css/main.50d62da5.chunk.css"
+  },
+  {
+    "revision": "ee538135e2fc4a61c50b",
+    "url": "/static/js/2.e8a63c2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e8a63c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "667b3871e1bd758b933b",
+    "url": "/static/js/main.7a469778.chunk.js"
+  },
+  {
+    "revision": "54f30fcaea9500ce404a",
+    "url": "/static/js/runtime-main.b4c216f6.js"
+  }
+]);
